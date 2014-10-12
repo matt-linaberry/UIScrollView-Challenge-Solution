b88d4ae4ceb4f709f28b67e716e106f26dfadbaa
Matt Linaberry <matt.linaberry@we-ics.com>
2014-10-12 09:54:11 -0400

    Initial Commit

---
+++ UIScrollView Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIScrollView Challenge Solution
+//
+//  Created by Matthew Linaberry on 10/12/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIScrollView Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIScrollView Challenge Solution
+//
+//  Created by Matthew Linaberry on 10/12/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
